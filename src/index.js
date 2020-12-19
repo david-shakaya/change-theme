1d76e0643f1f8e4878b2481cf9e6c7ca8fd5d08f
david-shakaya <devidoz666@gmail.com>
2020-12-20 00:29:17 +0200

    + проблема с логико

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { is } from 'core-js/fn/object';
+
 import '../src/styles.css'
 
 
@@ -26,33 +26,56 @@ const boxGreen = document.querySelector('.box-green-js');
 
 let delta = 0
 let beta = 0
-let isActive;
+let idTime;
+let idTime2;
 const defBox = () => box.style.transform = `translate(${delta = randomNumber()}px, ${402}px)`;
 boxGreen.style.transform = `translate(${beta = randomNumber()}px, ${402}px)`;
+
         defBox()
 
 
 btnStartRef.addEventListener('click', startGame)
-function startGame () {
- 
+function startGame() {
+
+    
     box.classList.add('move')
     box.style.transform = `translate(${delta}px, ${-80}px)`;
     box.addEventListener('click', removeClass)
     
+
+         
+    idTime = setTimeout(function () {
+        console.log(idTime);
+          defBox()
+             box.classList.remove('move')
+             boxGreen.classList.add('move')
+              boxGreen.style.transform = `translate(${beta}px, ${-80}px)`;
+              box.addEventListener('click', removeClass)
+        }, 2500);    
+    
 }
-    function removeClass() {
-            defBox()
+function removeClass(e) {
+    if (e) {
+        clearTimeout(idTime)
+        }
+        defBox()
         box.classList.remove('move')
         boxGreen.classList.add('move')
-        isActive = true
-        boxGreen.style.transform = `translate(${beta}px, ${-80}px)`;
+    boxGreen.style.transform = `translate(${beta}px, ${-80}px)`;
+    
+
+
+    
     }
 
-if (isActive) {
+
         boxGreen.addEventListener('click', removeClassNew)
-}
+
 
 function removeClassNew() {
+
+
     boxGreen.classList.remove('move')
-        box.classList.add('move')
+     box.style.transform = `translate(${delta}px, ${-80}px)`;
+    box.classList.add('move')
 }
