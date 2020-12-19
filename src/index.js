58ac178b7fd1f17c8870a939a5179f1d8aae72db
david-shakaya <devidoz666@gmail.com>
2020-12-19 23:35:03 +0200

    + Главная логика

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { is } from 'core-js/fn/object';
 import '../src/styles.css'
 
 
@@ -13,18 +14,45 @@ const bodyRef = document.querySelector('body');
 
 
 divGameAreaRef.insertAdjacentHTML('beforebegin', '<div class="box-red-js " ></div>');
+divGameAreaRef.insertAdjacentHTML('beforebegin', '<div class="box-green-js " ></div>');
 const box = document.querySelector('.box-red-js');
+const boxGreen = document.querySelector('.box-green-js');
 
+
+// const x = Math.floor(Math.random() * 780);
+// const newX = Math.floor(Math.random() * 780);
+
+    const randomNumber = () => Math.floor(Math.random() * 780);
+
+let delta = 0
+let beta = 0
+let isActive;
+const defBox = () => box.style.transform = `translate(${delta = randomNumber()}px, ${402}px)`;
+boxGreen.style.transform = `translate(${beta = randomNumber()}px, ${402}px)`;
+        defBox()
 
 
 btnStartRef.addEventListener('click', startGame)
 function startGame () {
-console.log(box);
-// box.classList.remove('box-red-js')
+ 
     box.classList.add('move')
-
+    box.style.transform = `translate(${delta}px, ${-80}px)`;
     box.addEventListener('click', removeClass)
-    function removeClass () {
-         box.classList.remove('move')
+    
+}
+    function removeClass() {
+            defBox()
+        box.classList.remove('move')
+        boxGreen.classList.add('move')
+        isActive = true
+        boxGreen.style.transform = `translate(${beta}px, ${-80}px)`;
     }
+
+if (isActive) {
+        boxGreen.addEventListener('click', removeClassNew)
+}
+
+function removeClassNew() {
+    boxGreen.classList.remove('move')
+        box.classList.add('move')
 }
