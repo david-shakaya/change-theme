a7f5bed935c4c7dc4a7a7842a5d611ea586f1622
david-shakaya <devidoz666@gmail.com>
2021-01-09 19:01:57 +0200

    создание боксов перенесено в отд.файл

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-
 import '../src/styles.css'
-import {refs} from './js/refs.js'
+import { refs } from './js/refs.js'
+import {createAndRemyveBox,createAndRemyveGreenBox,createAndRemyveBlueBox,createAndRemyvePurpleBox,defBox,defBoxGren,defBoxBlue,defboxPurple} from './js/createRemyveBox.js'
 
 
 const formRef =()=> document.querySelector('.form-action');
@@ -18,6 +18,9 @@ const boxGreen = document.querySelector('.box-green-js');
 const boxBlue = document.querySelector('.box-blue-js');
 const boxPurple = document.querySelector('.box-purple-js');
 const textAnimationRef = () => document.querySelector('.span-js'); //ищет класс
+// 1) Експортировать таймер отсюда
+// експортировать отсюда showSpan,countsPoints,defboxPurple
+
 
 //Добавля в локал хран при повторном захлде
 const nameUser = localStorage.getItem('nameUser');
@@ -26,7 +29,7 @@ const pointUser = localStorage.getItem('points');
 refs.ilListPlayersRef.insertAdjacentHTML('beforeend', `<li class="list-item-players">${nameUser}: ${pointUser} очков</li>`)
 
 
-const randomNumber = () => Math.floor(Math.random() * 655);
+
 /* padStart()
   * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
    */
@@ -34,10 +37,6 @@ function pad(value) {
     return String(value).padStart('2',0)
 }
 
-let delta = 0
-let beta = 0
-let zeta = 0
-let kappa= 0
 let timer = 0
 let points = 0;
 let remainingTime = 5
@@ -46,10 +45,7 @@ let idInterval = null
 let startIsActive = null
 let stopIsActive =null
 // Рандомное местоположение
-const defBox = () => box.style.transform = `translate(${delta = randomNumber()}px, ${402}px)`;
-const defBoxGren = () => boxGreen.style.transform = `translate(${beta = randomNumber()}px, ${402}px)`;
-const defBoxBlue = () => boxBlue.style.transform = `translate(${zeta = randomNumber()}px, ${402}px)`;
-const defboxPurple = () => boxPurple.style.transform = `translate(${kappa = randomNumber()}px, ${402}px)`;
+
 // const defBoxPurple
 
 
@@ -99,8 +95,7 @@ function showSpan(e) {
     
 }
 
-function startGame(e) {
-
+function startGame() {
     if (startIsActive) {
         return
     }
@@ -112,55 +107,11 @@ function startGame(e) {
   
 }
 
-function removeClass(e) {
-    showSpan(e)
-    if (e) {
-        console.log(e);
-        countsPoints()
-    }
-    defBox() // Местоположение перед выездом
-    
-
-    box.classList.remove('move') 
-    
-
-}
-function removeClassGreen(e) {
-    showSpan(e)
-    if (e) {
-        console.log(e);
-        countsPoints()
-    }
-    defBoxGren()
-    boxGreen.classList.remove('move-green') 
-
-}
-
-function removeClassBlue(e) {
-    showSpan(e)
-    if (e) {
-        console.log(e);
-        countsPoints()
-    }
-    defBoxBlue()
-    boxBlue.classList.remove('move-blue') 
-}
-// boxPurple
-function removeClassPurple(e) {
-    showSpan(e)
-    if (e) {
-        console.log(e);
-        countsPoints()
-    }
-    defboxPurple()
-    boxPurple.classList.remove('move-purple') 
-}
-
 
 
 function startTimer() {
          idInterval = setInterval(() => {
-     if (remainingTime === 4) {
+     if (remainingTime === 0||timer ===30) {
          onOpenModal()
          defBox()
          box.classList.remove('move')
@@ -198,59 +149,6 @@ function countsPoints() {
 
     refs.spanRemainingTimeRef.textContent = pad(remainingTime)
     refs.spanPoints.textContent = points
-}
-
-function createAndRemyveBox() {
-
-
- if (timer === 1||timer === 4 || timer === 7 || timer === 10 || timer === 13||timer===16 || timer ===19 || timer===22 || timer===25 || timer===28) {
-    box.classList.add('move')
-     box.addEventListener('mousedown', removeClass)
-     box.style.transform = `translate(${delta}px, ${-25}px)`;
-    }
-     if (timer ===3|| timer === 6 || timer === 9 || timer === 12||timer===15 || timer ===18 || timer===21 || timer===24 || timer===27 ||timer===30) {
-         removeClass()   
-         box.addEventListener('mousedown', removeClass)
-         
-        }
-}
-function createAndRemyveGreenBox() {
-if (timer === 2||timer === 5 || timer === 8 || timer === 11 || timer === 14||timer===17 || timer ===20 || timer===23 || timer===26 ) {
-    boxGreen.classList.add('move-green')
-     boxGreen.addEventListener('mousedown', removeClassGreen)
-    boxGreen.style.transform = `translate(${beta}px, ${-25}px)`; //Двигает зеленый бокс вверх
-
-    }
-     if ( timer ===4|| timer === 7|| timer === 10 || timer === 13||timer===16 || timer ===19 || timer===22 || timer===25 || timer===28) {
-         removeClassGreen()   
-    boxGreen.removeEventListener('mousedown', removeClassGreen)     
-        }
-}
-
-
-function createAndRemyveBlueBox() {
-if (timer === 3||timer === 5 || timer === 7 || timer === 9 || timer === 11||timer===13 || timer ===15 || timer===17 || timer===19||timer===21 || timer ===23 || timer===25 || timer===27|| timer===29 ) {
-    boxBlue.classList.add('move-blue')
-    boxBlue.addEventListener('mousedown', removeClassBlue)
-    boxBlue.style.transform = `translate(${zeta}px, ${-25}px)`; //Двигает зеленый бокс вверх
-    }
-     if (timer === 4 || timer === 6|| timer === 8 || timer === 10||timer===12 || timer ===14 || timer===16 || timer===18 || timer===20 || timer ===22 || timer===24 || timer===26 || timer===28|| timer ===30) {
-         removeClassBlue()   
-         boxBlue.removeEventListener('mousedown', removeClassBlue)
-        }
-}
-
-// ____________________________
-function createAndRemyvePurpleBox() {
-if (timer === 1||timer ===4 || timer === 7 || timer === 10 || timer === 13||timer===16 || timer ===19 || timer===22 || timer===25 || timer===28 ) {
-    boxPurple.classList.add('move-purple')
-    boxPurple.addEventListener('mousedown', removeClassPurple)
-    boxPurple.style.transform = `translate(${kappa}px, ${-25}px)`; //Двигает зеленый бокс вверх
-    }
-     if (timer ===3|| timer === 6 || timer === 9 || timer === 12||timer===15 || timer ===18 || timer===21 || timer===24 || timer===27 ||timer===30) {
-         removeClassPurple()   
-         boxPurple.removeEventListener('mousedown', removeClassPurple)
-        }
 }
 
 
@@ -335,3 +233,4 @@ function addNameInTable(nameUser,points ) {
      
     refs.ilListPlayersRef.insertAdjacentHTML('beforeend', `<li class="list-item-players">${nameUser}: ${points} очков</li>`)
 }
+export {timer,showSpan,countsPoints,defboxPurple,box,boxGreen,boxBlue,boxPurple,defBox,defBoxGren,defBoxBlue}
---
+++ src/js/createRemyveBox.js
@@ -0,0 +1,109 @@
+import {timer,showSpan,countsPoints,box,boxGreen,boxBlue,boxPurple} from '../index'
+
+const randomNumber = () => Math.floor(Math.random() * 655);
+let delta = 0
+let beta = 0
+let zeta = 0
+let kappa = 0
+
+
+const defBox = () => box.style.transform = `translate(${delta = randomNumber()}px, ${402}px)`;
+const defBoxGren = () => boxGreen.style.transform = `translate(${beta = randomNumber()}px, ${402}px)`;
+const defBoxBlue = () => boxBlue.style.transform = `translate(${zeta = randomNumber()}px, ${402}px)`;
+const defboxPurple = () => boxPurple.style.transform = `translate(${kappa = randomNumber()}px, ${402}px)`;
+
+function createAndRemyveBox() {
+ if (timer === 1||timer === 4 || timer === 7 || timer === 10 || timer === 13||timer===16 || timer ===19 || timer===22 || timer===25 || timer===28) {
+    box.classList.add('move')
+     box.addEventListener('mousedown', removeClass)
+     box.style.transform = `translate(${delta}px, ${-25}px)`;
+    }
+     if (timer ===3|| timer === 6 || timer === 9 || timer === 12||timer===15 || timer ===18 || timer===21 || timer===24 || timer===27 ||timer===30) {
+         removeClass()   
+         box.addEventListener('mousedown', removeClass)
+         
+        }
+}
+
+function createAndRemyveGreenBox() {
+if (timer === 2||timer === 5 || timer === 8 || timer === 11 || timer === 14||timer===17 || timer ===20 || timer===23 || timer===26 ) {
+    boxGreen.classList.add('move-green')
+     boxGreen.addEventListener('mousedown', removeClassGreen)
+    boxGreen.style.transform = `translate(${beta}px, ${-25}px)`; //Двигает зеленый бокс вверх
+
+    }
+     if ( timer ===4|| timer === 7|| timer === 10 || timer === 13||timer===16 || timer ===19 || timer===22 || timer===25 || timer===28) {
+         removeClassGreen()   
+    boxGreen.removeEventListener('mousedown', removeClassGreen)     
+        }
+}
+
+
+function createAndRemyveBlueBox() {
+if (timer === 3||timer === 5 || timer === 7 || timer === 9 || timer === 11||timer===13 || timer ===15 || timer===17 || timer===19||timer===21 || timer ===23 || timer===25 || timer===27|| timer===29 ) {
+    boxBlue.classList.add('move-blue')
+    boxBlue.addEventListener('mousedown', removeClassBlue)
+    boxBlue.style.transform = `translate(${zeta}px, ${-25}px)`; //Двигает зеленый бокс вверх
+    }
+     if (timer === 4 || timer === 6|| timer === 8 || timer === 10||timer===12 || timer ===14 || timer===16 || timer===18 || timer===20 || timer ===22 || timer===24 || timer===26 || timer===28|| timer ===30) {
+         removeClassBlue()   
+         boxBlue.removeEventListener('mousedown', removeClassBlue)
+        }
+}
+
+// ____________________________
+function createAndRemyvePurpleBox() {
+if (timer === 1||timer ===4 || timer === 7 || timer === 10 || timer === 13||timer===16 || timer ===19 || timer===22 || timer===25 || timer===28 ) {
+    boxPurple.classList.add('move-purple')
+    boxPurple.addEventListener('mousedown', removeClassPurple)
+    boxPurple.style.transform = `translate(${kappa}px, ${-25}px)`; //Двигает зеленый бокс вверх
+    }
+     if (timer ===3|| timer === 6 || timer === 9 || timer === 12||timer===15 || timer ===18 || timer===21 || timer===24 || timer===27 ||timer===30) {
+         removeClassPurple()   
+         boxPurple.removeEventListener('mousedown', removeClassPurple)
+        }
+}
+
+
+function removeClass(e) {
+    showSpan(e)
+    if (e) {
+        console.log(e);
+        countsPoints()
+    }
+    defBox() // Местоположение перед выездом
+    box.classList.remove('move') 
+    
+}
+function removeClassGreen(e) {
+    showSpan(e)
+    if (e) {
+        console.log(e);
+        countsPoints()
+    }
+    defBoxGren()
+    boxGreen.classList.remove('move-green') 
+
+}
+
+function removeClassBlue(e) {
+    showSpan(e)
+    if (e) {
+        console.log(e);
+        countsPoints()
+    }
+    defBoxBlue()
+    boxBlue.classList.remove('move-blue') 
+}
+// boxPurple
+function removeClassPurple(e) {
+    showSpan(e)
+    if (e) {
+        console.log(e);
+        countsPoints()
+    }
+    defboxPurple()
+    boxPurple.classList.remove('move-purple') 
+}
+
+export {createAndRemyveBox,createAndRemyveGreenBox,createAndRemyveBlueBox,createAndRemyvePurpleBox,defBox,defBoxGren,defBoxBlue,defboxPurple}
